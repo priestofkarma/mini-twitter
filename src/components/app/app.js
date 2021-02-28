903a210a40f8d6fec0ee17ad61fb5bdb1f639d21
Priestoflarma <priestofkarma@gmail.com>
2021-02-28 18:57:11 +0200

    create dynamic post-list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form/post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        { label: 'Going to learn React', important: true, id: "wefsd" },
+        { label: 'That is so good', important: false, id: "fdg" },
+        { label: 'I need a break...', important: false, id: "nbvn" }
+    ]
+
     return (
         <div className="app">
             <AppHeader />
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel />
                 <PostStatusFilter />
             </div>
-            <PostList />
+            <PostList posts={data} />
             <PostAddForm />
 
         </div>
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -5,12 +5,26 @@ import './post-list.css';
 
 import PostListItem from '../post-list-item/post-list-item';
 
-const PostList = () => {
+const PostList = ({ posts }) => {
+
+    const elements = posts.map((item) => {
+        const { id, ...itemProps } = item;
+
+        return (
+            /* <li className="list-group-item">
+                <PostListItem
+                    label={item.label}
+                    important={item.important} />
+            </li> */
+            <li key={id} className="list-group-item">
+                <PostListItem {...itemProps} />
+            </li>
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <PostListItem />
-            <PostListItem />
-            <PostListItem />
+            {elements}
         </ul>
     )
 }
