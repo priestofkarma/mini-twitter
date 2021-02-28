96b0c3095179f5b0257d70b618ae557819d55d2a
Priestoflarma <priestofkarma@gmail.com>
2021-02-28 15:06:52 +0200

    add application components

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Priestofkarma</h1>
+            <h2>5 записей, из них понравилось: 0</h2>
+
+        </div>
+    )
+}
+
+export default AppHeader;
