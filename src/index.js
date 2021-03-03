cca8018f8287ea3a0a10d917e27eb3c64b6c32ed
Priestoflarma <priestofkarma@gmail.com>
2021-03-04 00:54:42 +0200

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './components/app/';
+// import App from './components/app';
+import App from './components/app/app';
 // import AppHeader from './components/app-header/app-header';
 
 
