d22b65386f07956ef1f9a88fa4651cc4e56f350c
Priestoflarma <priestofkarma@gmail.com>
2021-04-14 20:47:35 +0300

    refactoring func to class and add filter

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './post-status-filter.css';
 
-import {Button} from 'reactstrap'
+// import {Button} from 'reactstrap'
 
 
-const PostStatusFilter = () => {
-    return(
-        <div className="btn-group">
-            <Button color="info">Все</Button>
-            <button type="button" className="btn btn-outline-secondary">Понравилось</button>
-        </div>
-    )
+export default class PostStatusFilter extends Component{
+    constructor(props) {
+        super(props);
+        this.buttons = [
+            { name: 'all', label: 'Все'},
+            { name: 'like', label: 'Понравилось'}
+        ]
+    }
+    render() {
+
+        const buttons = this.buttons.map(({ name, label }) => {
+            const {filter, onFilterSelect} = this.props;
+            const active = filter === name;
+            const color = active ? 'btn-info' : 'btn-outline-secondary'
+            return (
+                // <Button  color={color.color} key={name}>{label}</Button>
+                <button 
+                    key={name} 
+                    type="button" 
+                    className={`btn ${color}`}
+                    onClick={() => onFilterSelect(name)}
+                >{label}</button>
+            )
+        });
+
+        return (
+            <div className="btn-group">
+                {buttons}
+                {/* <button type="button" className="btn btn-outline-secondary">Понравилось</button> */}
+            </div>
+        )
+    }
 }
-
-export default PostStatusFilter;
